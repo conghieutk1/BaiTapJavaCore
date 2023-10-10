3747373772216881a96fc5e524a953526782f866
Hiếu Đặng <hieu37.nghiadung@gmail.com>
2023-10-10 09:33:09 +0700

    10/10/2023

--- src/main/java/BaiTap13/Main.java
+++ src/main/java/BaiTap13/Main.java
@@ -1,23 +1,47 @@
 package BaiTap13;
 
+import BaiTap13.Entity.*;
+import BaiTap13.Service.FunctionService;
+import BaiTap13.Service.Interface.IFunctionService;
+import BaiTap13.Service.Interface.IValidatation;
+import BaiTap13.Service.Validation;
+
 public class Main {
+
     public static void main(String[] args) {
-        Intern intern1 = new Intern(12, "Hiếu Đặng", "12/09/2002", "0123322321", "hieu@gmail.com", EmployeeType.INTERN, "IT", "20231", "HUST");
-        Intern intern2 = new Intern(12, "Hiếu Đặng", "12/09/2002", "0123322321", "hieu@gmail.com", EmployeeType.INTERN, "IT", "20231", "HUST");
-        Fresher fresher = new Fresher(12, "Hiếu Đặng", "12/09/2002", "0123322321", "hieu@gmail.com", EmployeeType.FRESHER, "15/8/2023", "Giỏi", "HUST");
-        Experience experience = new Experience(12, "Hiếu Đặng", "12/09/2002", "0123322321", "hieu@gmail.com", EmployeeType.INTERN, 5, "Lập trình Web");
+        IFunctionService functionService = new FunctionService();
+        Intern intern1 = new Intern(1, "intern1", "05/05/2003", "0922122321", "kan@gmail.com", EmployeeType.INTERN, "Electronic", "20221", "HUST");
+        Intern intern2 = new Intern(2, "intern2", "12/09/2002", "0123322321", "asfasc@gmail.com", EmployeeType.INTERN, "IT", "20231", "HUST");
+        Fresher fresher = new Fresher(3, "fresher", "17/01/2006", "0823322321", "asdec@gmail.com", EmployeeType.FRESHER, "15/8/2023", "Giỏi", "NEU");
+        Experience experience = new Experience(4, "experience", "17/03/2000", "0723322321", "vsdg@gmail.com", EmployeeType.EXPERIENCE, 5, "Lập trình Web");
 
-        System.out.println(intern1.ShowInfo());
-        System.out.println(intern2.ShowInfo());
-        System.out.println(intern2.ShowMe());
-
-        System.out.println(fresher.ShowInfo());
-        System.out.println(fresher.ShowMe());
-
-        System.out.println(experience.ShowInfo());
-        System.out.println(experience.ShowMe());
-
-
-
+        EmployeeManagement employeeManagement = new EmployeeManagement();
+        employeeManagement.insertEmployee(intern1);
+        employeeManagement.insertEmployee(intern2);
+        employeeManagement.insertEmployee(fresher);
+        employeeManagement.insertEmployee(experience);
+//        employeeManagement.showAllEmployeeByType(EmployeeType.INTERN);
+//        employeeManagement.deleteEmployeeByType(EmployeeType.INTERN);
+//        employeeManagement.showAllEmployee();
+        char a;
+        do {
+            a = functionService.chooseFunctionMainMenu();
+            switch (a) {
+                case '1' -> {
+                    functionService.chucNang1(employeeManagement);
+                }
+                case '2' -> {
+                    functionService.chucNang2(employeeManagement);
+                }
+                case '3' -> {
+                    functionService.chucNang3(employeeManagement);
+                }
+                case '4' -> {
+                    functionService.chucNang4(employeeManagement);
+                }
+                default -> {
+                }
+            }
+        } while (a != '0');
     }
 }
